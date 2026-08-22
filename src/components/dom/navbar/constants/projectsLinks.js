fba79dc807916af45789e29d25f856d7b8422904
Arsh_Singh <arshdeepsingh92283@gmail.com>
2026-08-22 17:34:35 +0530

    Correct Project links and structure

--- src/components/dom/navbar/constants/projectsLinks.js
+++ src/components/dom/navbar/constants/projectsLinks.js
@@ -1,10 +1,10 @@
 const projectsLinks = [
-  { title: 'GPT-Teacher', href: '/projects/gpt-teacher' },
+  { title: 'DocFlow', href: '/projects/docflow' },
+  { title: 'VigilVault', href: '/projects/vigilvault' },
   { title: 'CourseSphere', href: '/projects/coursesphere' },
   { title: 'FinEdge', href: '/projects/finedge' },
-  { title: 'VigilVault', href: '/projects/vigilvault' },
-  { title: 'AI Gallery App', href: '/projects/aigalleryapp' },
   { title: 'E-Commerce Platform', href: '/projects/ecommerce' },
-  { title: 'DocFlow', href: '/projects/docflow' },
+  { title: 'AI Gallery App', href: '/projects/aigalleryapp' },
+  { title: 'GPT-Teacher', href: '/projects/gpt-teacher' },
 ];
 export default projectsLinks;
